17af1dde11461952668cc65a8e3ecd1a89d34b74
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-18 16:00:55 +0000

    Add marker to MapView and update Avatar component

--- Pages/HomeScreen.js
+++ Pages/HomeScreen.js
@@ -1,19 +1,30 @@
-import { View, Text, SafeAreaView,StyleSheet,Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView,StyleSheet,Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from "@react-navigation/native";
 import MapView,{PROVIDER_GOOGLE} from 'react-native-maps';
-import Avatar from '../components/Avatar';
+import { Marker } from 'react-native-maps';
+import { Avatar } from "@react-native-material/core";
 export default function HomeScreen() {
   return (
     <SafeAreaView  style={styles.home}>
-      <MapView style={styles.map}></MapView>
+      <MapView style={styles.map}>
+        <Marker/>
+      </MapView>
       <View style={styles.float}>
         <View style={styles.logoContainer}>
           <Image source={require('../Images/EcoHaul.png')} style={styles.imageStyle}/>
-          <Avatar/>
+          <Avatar label="Emmanuel Nyatepe" size={45} style={{marginRight:"3%",}}/>
         </View>
         <TextInput style={styles.input}/>
-  
+        <View style={styles.requestContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.txt1}>Put in a Haul Request</Text>
+            <Text style={styles.txt2}>Select a location on the map for waste pickup</Text>
+          </View>
+          <TouchableOpacity style={styles.requestBtn}>
+            <Text style={{color:"#fff"}}>Make Request</Text>
+          </TouchableOpacity>
+        </View>
         </View>
     </SafeAreaView>
   )
@@ -30,7 +41,7 @@ const styles = StyleSheet.create({
       position:"absolute",
       width:"95%",
       height:"25%",
-      backgroundColor:"#F4F6F6",
+      backgroundColor:"#eeeee4",
       alignSelf:"center",
       top:"5%",
       borderRadius:10,
@@ -56,6 +67,44 @@ const styles = StyleSheet.create({
       width:"80%",
       height:"25%",
       borderRadius:10,
-      backgroundColor:"dodgerblue"
+      backgroundColor:"#fff"
+    },
+    requestContainer:{
+      width:"95%",
+      height:"40%",
+      marginTop:"1%",
+      display:"flex",
+      flexDirection:"row",
+      alignItems:"center",
+      justifyContent:"space-between",
+    },
+    textContainer:{
+      width:"60%",
+      height:"70%",
+      display:"flex",
+      flexDirection:"column",
+      justifyContent:"space-around",
+    },
+    requestBtn:{
+      width:"35%",
+      height:"60%",
+      backgroundColor:"#179A72",
+      borderRadius:4,
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      marginRight:"2%",
+      color:"#fff"
+    },
+    txt1:{
+      color:"black",
+      fontSize:19,
+      fontWeight:"600"
+    },
+    txt2:{
+      color:"black",
+      fontSize:9,
+      fontWeight:"400",
+      opacity:0.5
     }
 })
--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Avatar() {
   return (
     <View style={styles.avatar}>
-      
+      <Text style={{textAlign:"center",fontSize:25,fontWeight:700}}>EN</Text>
     </View>
   )
 }
@@ -16,7 +16,10 @@ const styles = StyleSheet.create({
         borderRadius:90,
         backgroundColor:"red",
         borderWidth:1,
-        borderColor:"#000",
-        marginRight:"2%"
+        borderColor:"#179A72",
+        marginRight:"2%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
     }
 })
