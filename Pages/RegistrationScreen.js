eeedc8f86d8c8e7be6956a1a8abc453e5b2c5c31
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-19 18:09:26 +0000

    Add user registration functionality

--- Pages/RegistrationScreen.js
+++ Pages/RegistrationScreen.js
@@ -1,39 +1,56 @@
-
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, SafeAreaView, KeyboardAvoidingView, View, Text, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
 import {UserIcon,HashtagIcon,EnvelopeIcon,LockClosedIcon } from 'react-native-heroicons/outline'
 
 const RegistrationScreen = () => {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [phone, setPhone] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = () => {
+   const userData = {
+      username,
+      email,
+      phone,
+      password
+    }
+    console.log(userData);
+    
+  }
+
   return (
     <SafeAreaView style={styles.safeArea}>
         <View style={styles.logoContainer}>
           <Image source={require('../Images/EcoHaul.png')} style={styles.imageStyle}/>
-          <Text style={styles.logoText} >Let's get you started. Create an account to start with us</Text>
+          <Text  style={styles.logoText} >Let's get you started. Create an account to start with us</Text>
         </View>
         <View style={styles.formContainer}>
           <View style={styles.inputGroup}>
             <UserIcon color="black" size={20} style={styles.sideIcon}/>
-            <TextInput style={styles.input}/>
+            <TextInput onChangeText={setUsername} style={styles.input}/>
           </View>
           <View style={styles.inputGroup}>
             <HashtagIcon color="black" size={20} style={styles.sideIcon}/>
-            <TextInput style={styles.input}/>
+            <TextInput onChangeText={setPhone} style={styles.input}/>
           </View>
           <View style={styles.inputGroup}>
           <EnvelopeIcon color="black" size={20} style={styles.sideIcon}/>
-          <TextInput style={styles.input}/>
+          <TextInput onChangeText={setEmail}  style={styles.input}/>
           </View>
           <View style={styles.inputGroup}>
           <LockClosedIcon color="black" size={20} style={styles.sideIcon}/>
-          <TextInput style={styles.input}/>
+          <TextInput  onChangeText={setPassword} style={styles.input}/>
           </View>
           <View style={styles.inputGroup}>
           <LockClosedIcon color="black" size={20} style={styles.sideIcon}/>
           <TextInput style={styles.input}/>
           </View>
           <Text style={{textAlign:"center"}}>By continuing you agree with our {"\n"}Terms and Condotions</Text>
-          <TouchableOpacity style={styles.registerBtn} onPress={() => navigation.navigate('login')}>
+          <TouchableOpacity style={styles.registerBtn} onPress={handleRegister}>
              <Text style={{color:"#fff",fontSize:16}}>Register</Text>
           </TouchableOpacity>
           <Text style={styles.signTxt} onPress={()=>navigation.navigate('login')}>Have an account? Sign in </Text>
