4e00c2dc2712155bd0e4c3a7ac87c83d2b49d81f
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-20 08:27:54 +0000

    Update function to handle edge case

--- Navigations.js
+++ Navigations.js
@@ -16,7 +16,6 @@ export default function Navigations() {
       <Stack.Screen options={{headerShown: false}} name="SPLASH" component={SplashScreen} />
         <Stack.Screen options={{headerShown: false}} name="login" component={Logincreen} />
         <Stack.Screen options={{headerShown: false}} name="signIn" component={RegistrationScreen} />
-        <Stack.Screen options={{headerShown: false}} name="homescreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
