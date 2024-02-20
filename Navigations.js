669b59383a61896f1c4897e5d938298be985e480
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-20 09:00:12 +0000

    Add AuthProvider to Navigations.js

--- Navigations.js
+++ Navigations.js
@@ -1,13 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
+import { AuthProvider } from './context/AuthContext';
 
 export default function Navigations() {
   return (
-    <NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
 
       <AuthStack />
       {/* <AppStack /> */}
-    </NavigationContainer>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
