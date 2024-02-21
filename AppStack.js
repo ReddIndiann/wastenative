8d7d6ed306a07a79ec8081c5a5222ac02349ba07
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-21 06:42:44 +0000

    Refactor Navigations.js to include userInfo in AuthContext

--- AppStack.js
+++ AppStack.js
@@ -4,9 +4,9 @@ import UserInfo from './Pages/UserInfo';
 import DriverTabs from './DriverTabs/layout';
 const Stack = createNativeStackNavigator();
 
-const AppStack = () => {
+const AppStack = ({ initialRouteName }) => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName={initialRouteName}>
       <Stack.Screen options={{ headerShown: false }} name="Tabs" component={Tabs} />
       <Stack.Screen options={{ headerShown: false }} name="DriverTabs" component={DriverTabs} />
     </Stack.Navigator>
--- Navigations.js
+++ Navigations.js
@@ -6,15 +6,25 @@ import { ActivityIndicator, View } from 'react-native';
 import { AuthContext } from './context/AuthContext';
 
 export default function Navigations() {
-  const { isLoading, userToken } = useContext(AuthContext);
+  const { isLoading, userToken,userInfo } = useContext(AuthContext);
   if (isLoading) {
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <ActivityIndicator size="large" color="#0000ff" />
     </View>
   }
+  const renderAppStack = () => {
+    if (userInfo?.role === 'user') {
+      return <AppStack initialRouteName="Tabs" />;
+    } else if (userInfo?.role === 'driver') {
+      return <AppStack initialRouteName="DriverTabs" />;
+    } else {
+      return <AuthStack />;
+    }
+  };
+
   return (
-      <NavigationContainer>
-        {userToken !==null ? <AppStack /> : <AuthStack />}
-      </NavigationContainer>
+    <NavigationContainer>
+      {userToken ? renderAppStack() : <AuthStack />}
+    </NavigationContainer>
   );
 }
