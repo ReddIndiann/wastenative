07527ff067be1f0cece8541304881ae385142062
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-21 07:36:32 +0000

    Refactor HomeScreen component to remove unnecessary code and set default region

--- DriverTabs/HomeScreen.js
+++ DriverTabs/HomeScreen.js
@@ -1,42 +1,16 @@
-import { StyleSheet, Text, View ,Platform, PermissionsAndroid} from 'react-native'
-import {useState,useEffect} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {useState} from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation';
 
 export default function HomeScreen() {
-    const [region, setRegion] = useState(null);
-
-    useEffect(() => {
-        if (Platform.OS === 'android') {
-            PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-            ).then(granted => {
-                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                    getCurrentLocation();
-                }
-            });
-        } else {
-            // For iOS, request permission in Info.plist
-            getCurrentLocation();
-        }
-    }, []);
+    const [region, setRegion] = useState({
+        latitude: 5.614818,
+        longitude: -0.205874,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
     
-    const getCurrentLocation = () => {
-        Geolocation.getCurrentPosition(
-            position => {
-                const currentRegion = {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                };
-                setRegion(currentRegion);
-            },
-            error => console.log(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-        );
-    };
-
+      
     const handleMapPress = (e) => {
         setCoordinate(e.nativeEvent.coordinate); // Store the coordinate object directly
         console.log(e.nativeEvent.coordinate);
@@ -44,11 +18,9 @@ export default function HomeScreen() {
 
   return (
     <View style={{flex:1}}>
-        {region && (
       <MapView onPress={handleMapPress} zoo initialRegion={region}  provider={PROVIDER_GOOGLE} showsUserLocation={true} followsUserLocation={true}  style={styles.map}>
         
       </MapView>
-        )}
     </View>
   )
 }
