6766190a7b4cfdb6f75648d576e79655a05fffeb
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-23 15:04:47 +0000

    Add user avatar and logout functionality to DriverInfo component

--- DriverTabs/DriverInfo.js
+++ DriverTabs/DriverInfo.js
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,SafeAreaView, Touchable, TouchableOpacity } from 'react-native'
+import {useContext,useState} from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { Avatar } from '@react-native-material/core';
 
 export default function DriverInfo() {
+  const {logout,userInfo}=useContext(AuthContext);
+  const username = userInfo?.username;
   return (
-    <View>
-      <Text>DriverInfo</Text>
-    </View>
+    <SafeAreaView style={styles.app}>
+      <Avatar size={150} name={username} round />
+      <Text>{username}</Text>
+      <TouchableOpacity style={styles.logoutBtn} onPress={()=>logout()}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+ app:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+ },
+ logoutBtn:{
+   backgroundColor:'dodgerblue',
+   borderRadius:5,
+   marginTop:20,
+   width:"50%",
+   height:"8%"
+ }
+})
