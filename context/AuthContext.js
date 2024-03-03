cffe76ff9fc423c8985953240d58562f0f7fe524
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-03-03 21:27:04 +0000

    Update API endpoint and adjust borderRadius in layout

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
     const [streamToken, setStreamToken] = useState(null);
 
     const fetchUserRequests = (email) => {
-        axios.get(`http://172.20.10.9:5000/api/request/userhistory?author=${email}`)
+        axios.get(`http://172.20.10.5:5000/api/request/userhistory?author=${email}`)
             .then(res => {
                 // Handle the response containing the requests
                 console.log("User requests:", res.data);
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
     const login = (email,password) => {
         console.log("Logging in");
         setLoading(true);
-        axios.post("http://172.20.10.9:5000/api/auth/login",{email,password})
+        axios.post("http://172.20.10.5:5000/api/auth/login",{email,password})
         .then(res=>{
             const { email, role, token, username,comAssociate ,phone,id,streamToken,areaAssigned} = res.data;
             console.log("API Response:", res.data);
@@ -84,7 +84,7 @@ export const AuthProvider = ({ children }) => {
     const completeRequest = async (requestId,comAssociate) => {
         console.log(requestId)
         try {
-            const response = await axios.post("http://172.20.10.9:5000/api/drivers/status", { requestId,comAssociate });
+            const response = await axios.post("http://172.20.10.5:5000/api/drivers/status", { requestId,comAssociate });
             console.log("Request completed:", response.data);
             // You can add logic here to update your state or UI based on the response
         } catch (error) {
